2dde5ec1d3d12e9012e6a30cbe9af02573e6e71b
Jeff <jeff.revesz@buzzfeed.com>
2015-01-11 13:34:00 -0500

    small formatting cleanup

--- public/js/controllers/posts.js
+++ public/js/controllers/posts.js
@@ -2,21 +2,21 @@
 angular.module('flapperNews').controller('MainController', [
   'posts',
   function (posts) {
-      var self = this;
-      self.posts = posts.posts;
-      self.addPost = function () {
-        console.log('A');
-        if (!self.title || self.title === '') { return; }
-        console.log('B');
-        self.posts.push({
-            title: self.title,
-            link: self.link,
-            upvotes: 0
-        });
-        self.title = '';
-        self.link = '';
-      };
-      self.incrementUpvotes = function (post) {
-          post.upvotes += 1;
-      };
+    var self = this;
+    self.posts = posts.posts;
+    self.addPost = function () {
+      console.log('A');
+      if (!self.title || self.title === '') { return; }
+      console.log('B');
+      self.posts.push({
+          title: self.title,
+          link: self.link,
+          upvotes: 0
+      });
+      self.title = '';
+      self.link = '';
+    };
+    self.incrementUpvotes = function (post) {
+        post.upvotes += 1;
+    };
   }]);
